2aff92396753a3bb898b54e78ec9b308a3605c29
Aaron Frazier Jr <afraz91@wgu.edu>
2023-05-06 12:42:40 -0700

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,27 @@
+ public class Exercise {
+     public static void print() {
+         //Defining all variables
+
+         int int_number = 1000;
+         char char_name = 'N';
+         float float_number = 10.292f;
+         double double_number = 0.00000000000512365123;
+         boolean bool_accept = true;
+
+
+         System.out.println("Value of Int is: ");
+         System.out.println(String.valueOf(int_number));
+
+         System.out.println("Value of Float is: ");
+         System.out.println(String.valueOf(float_number));
+
+         System.out.println("Value of Double is: ");
+         System.out.println(String.valueOf(double_number));
+
+         System.out.println("Value of Char is: ");
+         System.out.println(String.valueOf(char_name));
+
+         System.out.println("Value of Bool is: ");
+         System.out.println(String.valueOf(bool_accept));
+     }
+ }
