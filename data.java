662eac8e599a3bb529a77516a03e418285d25189
Aaron Frazier Jr <afraz91@wgu.edu>
2023-05-06 09:46:16 -0700

    Create data.java

---
+++ data.java
@@ -0,0 +1,9 @@
+// A short type is a 16 bit signed integer
+short age; // Stores from +32,767 to -32,768	
+
+// An int type is a 32 bit signed integer
+int age; // Stores from +2,147,483,647 to -2,147,483,648
+
+// A long type is a 64 bit signed integer
+long age; // Stores from +9,223,372,036,854,775,807 to -9,223,372,036,854,775,808
+
