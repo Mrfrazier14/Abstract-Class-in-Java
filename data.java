9dd65d281aba29d9cef2b8cd9cdc6cfb70760572
Aaron Frazier Jr <afraz91@wgu.edu>
2023-05-06 09:50:45 -0700

    Update data.java

--- data.java
+++ data.java
@@ -1,3 +1,4 @@
+// integer Examples
 // A short type is a 16 bit signed integer
 short age; // Stores from +32,767 to -32,768	
 
@@ -7,7 +8,7 @@ int age; // Stores from +2,147,483,647 to -2,147,483,648
 // A long type is a 64 bit signed integer
 long age; // Stores from +9,223,372,036,854,775,807 to -9,223,372,036,854,775,808
 
-// Floating points
+// Floating points Examples
 
 // Assigning a float to a radius of a circle
 
