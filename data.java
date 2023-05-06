753b6fc244269b9b49505150efc6a5658259a14b
Aaron Frazier Jr <afraz91@wgu.edu>
2023-05-06 09:53:41 -0700

    Update data.java

--- data.java
+++ data.java
@@ -30,3 +30,18 @@ public class Type_Double {
         System.out.println(radius);
     }
 }
+
+// Char Examples 
+
+public class Type_Char {
+    public static void main(String[] args) {
+        char letter = 'A';
+        System.out.println(letter);
+
+        char number_as_text = '6';
+        System.out.println(number_as_text);
+
+        char number = 65;
+        System.out.println(number);
+    }
+}
