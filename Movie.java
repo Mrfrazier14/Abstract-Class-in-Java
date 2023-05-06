31f1624dbc26419e28173f9cf9fc94f14a79418d
Aaron Frazier Jr <afraz91@wgu.edu>
2023-05-05 23:13:26 -0700

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void print() {
         // write your code here
         System.out.println("The movie quote is: My name is Bond. James Bond!");
+        
     }
 }
