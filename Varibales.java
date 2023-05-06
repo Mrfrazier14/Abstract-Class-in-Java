48e932059bb2b7e2d52d7354a1a0c8e37bd77d67
Aaron Frazier Jr <afraz91@wgu.edu>
2023-05-06 10:09:06 -0700

    Create Varibales.java

---
+++ Varibales.java
@@ -0,0 +1,41 @@
+// Declaring variables 
+<vaibale type> <variable identifier>
+
+// Examples of diff variables
+
+// Creating an age int
+int age;
+
+//  Intializing variables 
+
+// We have initialized the variable my_age with thw vaule of 13
+
+int my_age = 13;
+
+//Declaring multiple variables in a single line
+
+// Group Delarations
+int age, height;
+cahr letter, alpha;
+double radium, area;
+
+//Initializing multiple variables in a sinhkle line 
+
+// Groupe initializations 
+int age = 10, height = 5;
+double radius = 2.34, area = 4.55;
+
+
+// Last example
+class Variables {
+    public static void main( String args[] ) {
+        // Group initializations
+        int age = 10, height = 5;
+        double radius = 2.34, area = 4.55;
+        // Printing the variables
+        System.out.println( "age: " + age );
+        System.out.println( "height: " + height );
+        System.out.println( "radius: " + radius );
+        System.out.println( "area: " + area );
+    }
+}
