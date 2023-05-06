1d0d2c51332f0036a896a53a4799e8d46c9f7784
Aaron Frazier Jr <afraz91@wgu.edu>
2023-05-06 09:51:16 -0700

    Update data.java

--- data.java
+++ data.java
@@ -18,3 +18,13 @@ public class Type_Floats {
         System.out.println(radius);
     }
 }
+// Assigning a double to a radius of a circle
+
+public class Type_Double {
+    public static void main(String[] args) {
+        float radius1 = 0.00000000000863872078f;
+        System.out.println(radius1);
+        double radius = 0.00000000000863872078;
+        System.out.println(radius);
+    }
+}
