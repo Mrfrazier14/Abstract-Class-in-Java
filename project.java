88d98b69dc5d82381622b02d58503dc326f91de5
Aaron Frazier Jr <afraz91@wgu.edu>
2023-05-07 18:32:22 -0700

    Create project.java

---
+++ project.java
@@ -0,0 +1,27 @@
+public class Oper_Prec {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        int z = 7;
+        int w = 6;
+        int answer;
+
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        System.out.println("z: " + z);
+        System.out.println("w: " + w);
+
+        System.out.println("Calculating Expressions with Multiple Operators");
+
+        System.out.println("(x-z) + y*y = " + ((x - z) + y * y));
+        answer = (w / y + (x * z));
+
+        System.out.println("w/y + (x*z) = " + answer);
+    }
+}
+
+// This is the new area of how I would this and go for a stronger and better SWE in JAVA.
+
+//This is just one of the very few that I can actucally learn and grow up in IT.
+      
+    
