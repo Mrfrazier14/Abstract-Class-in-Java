48b7e35dad2ac6a8bb0d9a78fea0b76d0085fd86
Aaron Frazier Jr <afraz91@wgu.edu>
2023-05-08 23:14:37 -0700

    Update Lang.java

--- Lang.java
+++ Lang.java
@@ -13,4 +13,32 @@ public class absolute {
         System.out.println("Absolute value of 2: " + Math.abs(2));
     }
 }
+
+class log_op {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+
+        System.out.println("x is equal to: " + x);
+        System.out.println("y is equal to: " + y);
+
+        System.out.println("x is greater than y");
+        System.out.println(x > y);
+
+        System.out.println("x is less than y");
+        System.out.println(x < y);
+
+        System.out.println("x is greater than or equal to y");
+        System.out.println(x >= y);
+
+        System.out.println("y is less than or equal to y");
+        System.out.println(y <= y);
+
+        System.out.println("x is equal to y");
+        System.out.println(x == y);
+
+        System.out.println("x is not equal to y");
+        System.out.println(x != y);
+    }
+}
 // This is the math side of Java
