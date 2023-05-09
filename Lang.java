0574edc0690b52bbb04eac1165876bd46075ffc3
Aaron Frazier Jr <afraz91@wgu.edu>
2023-05-08 23:13:02 -0700

    Create Lang.java

---
+++ Lang.java
@@ -0,0 +1,10 @@
+public class exponent {
+    public static void main(String[] args) {
+        System.out.println("2 raised to the power 3 is " + Math.pow(2, 3));
+        System.out.println("Exponent squared is " + Math.exp(2));
+        System.out.println("The square root of 16 is " + Math.sqrt(16));
+        System.out.println("The cube root of 27 is " + Math.cbrt(27));
+    }
+}
+
+// This is the math side of Java
