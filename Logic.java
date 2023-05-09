fba0733196642ebe051b8acb7c9e343414fdaea5
Aaron Frazier Jr <afraz91@wgu.edu>
2023-05-09 13:34:09 -0700

    Create Logic.java

---
+++ Logic.java
@@ -0,0 +1,29 @@
+class log_op {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+
+        System.out.println("x is equal to: " + x);
+        System.out.println("y is equal to: " + y);
+
+        System.out.println("x is greater than y");
+        System.out.println(x > y);
+
+        System.out.println("x is less than y");
+        System.out.println(x < y);
+
+        System.out.println("x is greater than or equal to y");
+        System.out.println(x >= y);
+
+        System.out.println("y is less than or equal to y");
+        System.out.println(y <= y);
+
+        System.out.println("x is equal to y");
+        System.out.println(x == y);
+
+        System.out.println("x is not equal to y");
+        System.out.println(x != y);
+    }
+}
+
+// I added this for future use
