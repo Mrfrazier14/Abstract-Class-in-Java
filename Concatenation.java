21666bd2449f84ae4af264d07f95480282a536db
Aaron Frazier Jr <afraz91@wgu.edu>
2023-05-13 00:13:33 -0700

    Update Concatenation.java

--- Concatenation.java
+++ Concatenation.java
@@ -67,3 +67,15 @@ class split_string {
         System.out.println(greet.toLowerCase());
     }
 }
+// Length of a string
+class split_string {
+    public static void main(String[] args) {
+        String greet = "HeLlo WoRld";
+
+        //Returns new string in which all characters are converted to upper case
+        System.out.println(greet.toUpperCase());
+
+        //Returns new string in which all characters are converted to lower case
+        System.out.println(greet.toLowerCase());
+    }
+}
