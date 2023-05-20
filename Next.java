959f7848fc2bc4d26bd631b658b861464675e26c
Aaron Frazier Jr <afraz91@wgu.edu>
2023-05-20 00:47:39 -0700

    Create Next.java

---
+++ Next.java
@@ -0,0 +1,30 @@
+class conditional_exp {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        int answer;
+        // Using conditional expression 
+        answer = (x > y) ? x : y;
+        System.out.println("Answer using conditional: " + answer);
+        // The above code is equivalent to:
+        // Using the if-else method 
+        if (x > y) {
+            answer = x;
+        } else {
+            answer = y;
+        }
+        System.out.println("Answer using if-else: " + answer);
+    }
+}
+
+class coniional_exp {
+  public static void main(String[] args) {
+     int x = 20;
+     int y = 6;
+     int answer; 
+    // Using conitional expression
+    answer = (x > y ) ? x : y;
+    System.out.println("Answer using conditional: " + answe");
+      
+  }
+}
