c05d8d83cf5280aa706c202f5b945f12b56fd7e1
Aaron Frazier Jr <afraz91@wgu.edu>
2023-06-12 23:23:58 -0700

    Create Loop.java

---
+++ Loop.java
@@ -0,0 +1,10 @@
+class Loops {
+    public static void main(String args[]) {
+        int i = 0;
+        while (i < 10) // while loop runs till i is less then 10 just like in for loop
+        {
+            System.out.println("value of i =  " + i); //prints value of i
+            i++;
+        }
+    }
+}
